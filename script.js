170747455f67747cb451077b76a55ad3e73c0d78
Abin VK <vettikkadanabin@gmail.com>
2026-08-22 18:41:12 +0530

    project

---
+++ script.js
@@ -0,0 +1,15 @@
+// ================================
+// SkyCast - JavaScript
+// Step 2: DOM + Events
+// ================================
+
+
+// Select HTML elements
+
+const cityInput = document.getElementById("cityInput");
+const searchBtn = document.getElementById("searchBtn");
+
+const cityName = document.getElementById(cityName);
+const temperature = document.getElementById("temperature");
+const condition = document.getElementById("condition");
+const feelsLike = document.getElementById("feelsLike");
